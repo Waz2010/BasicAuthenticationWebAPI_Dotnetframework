77eee0e1a2306022fdc53a0fc5c2a5760b8f7584
waseem haider <waseemhaider1947@gmail.com>
2019-10-23 12:54:59 -0700

    This is employee services with login and log of and retrive emoplyee list from the db using Entity framework.

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{       
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDB4Entities entities = new EmployeeDB4Entities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
